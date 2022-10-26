986fe357a48b71b133f9bc4a85920ed630ebe541
Robson Rezende <85044649+robrezen@users.noreply.github.com>
2022-10-26 16:55:33 -0300

    chore: change Algorithms  POO for  structure

---
+++ algorithms.py
@@ -0,0 +1,38 @@
+def my_sort(book: list, order: object):
+    start, end = 0, len(book)
+    while start < end:
+        half = (start + end) // 2
+        if order.price < book[half].price:
+            end = half
+        else:
+            start = half + 1
+    book.insert(start, order)
+    return
+
+def serchBinary(book: list, order: object):
+    start, end = 0, len(book)-1
+    while start <= end:
+        half = (start + end) // 2
+        if order.price == book[half].price:
+            return half
+        elif order.price < book[half].price:
+            end = half - 1
+        else: 
+            start = half + 1
+    return "Not found"
+    
+#finds the order with the highest priority in the book considering the price of the order
+def HigherPriority(book: list, order: object):
+    position = len(book)-1
+    if position > 0:
+        prevPriceBook = book[position-1].price
+        while  prevPriceBook == order.price and position >= 1:
+            position = position -1
+            prevPriceBook = book[position].price
+    return position 
+
+def findOrder(id: int, book: list):
+    for position,orderBook in enumerate(book):
+        if orderBook.id == id:
+            return orderBook, position
+    return "Not found", print("Invalid ID or Order is not in the book anymore") 
---
+++ exchange.py
@@ -0,0 +1,96 @@
+from algorithms import *
+from order import Order
+from order_limit import OrderLimit
+from trading import Trading
+
+class Exchange:
+    def __init__(self, id_even=0, id_odd=-1, order=0):
+        self.id_even = id_even
+        self.id_odd = id_odd
+        self.order = order
+        self.__book_orders_sell = []
+        self.__book_orders_buy = []
+
+    #set id for the order, type order buy receive id even and sell id odd
+    def getId(self, side: str):
+        if side == "buy":
+            self.id_even = self.id_even + 2
+            return self.id_even
+        elif side == "sell":
+            self.id_odd = self.id_odd +2
+            return self.id_odd
+    def getOrder(self):
+        return self.order
+
+    def createLimitOrder(self, type_order: str, side: str, price: int, qty: int, id: int):
+        self.order = OrderLimit(type_order, side, price, qty, id)
+        print(f"Order created: {self.order.side} {self.order.qty} @ {self.order.price} identificador_{self.order.id}")
+    
+    def createOrderMarket(self, type_order: str, side: str, qty: int):
+        self.order = Order(type_order, side, qty)
+        print(f"Order created: {self.order.type_order} {self.order.side} {self.order.qty}")
+
+    def getOrderBook(self, side: str):
+        if side == "buy":
+            return self.__book_orders_buy, self.__book_orders_sell
+        elif side == "sell":
+            return self.__book_orders_sell, self.__book_orders_buy
+
+    def insertOrder(self, book: list):
+        my_sort(book, self.order)
+
+    def serchOrder(self, book: list):
+        return serchBinary(book, self.order)
+
+    def findBook(self, id: int):
+        if id % 2:
+            return self.__book_orders_sell
+        else:
+            return self.__book_orders_buy
+
+    def tradeOperation(self, order_in_book: object):
+        trade = Trading()
+        return trade.trade(order_in_book, self.order)
+    
+    def deleteOrder(self, position: int, book: list):
+        del book[position]
+
+    def changeOrder(self, id: int, new_id: int, book: list, price=0, qty=0):
+        book_order, position = findOrder(id, book)
+        if price != 0:
+            book_order.price = price
+        if qty != 0:
+            book_order.qty = qty
+        book_order.id = new_id
+        self.order = book_order
+        del book[position]
+        my_sort(book, self.order)
+        print(f"Order changed: {self.order.side} {self.order.qty} @ {self.order.price} identificador_{self.order.id}")
+
+    #standard set up of operations considering the type of operation
+    def constructor(self, order_str: str):
+        list_order = order_str.split()
+        typeOperation = list_order[0]
+        if typeOperation == "cancel" or typeOperation == "change":
+            id_string = list_order[2].split("_")
+            id = int(id_string[1])
+            if typeOperation == "change":
+                if len(list_order) == 6:
+                    qty = int(list_order[3])	
+                    price = int(list_order[5])
+                    return id, None, price, qty, typeOperation
+                elif len(list_order) == 5:
+                    price = int(list_order[4])
+                    return id, None, price, 0, typeOperation
+                elif len(list_order) == 4:
+                    qty = int(list_order[3])
+                    return id, None, 0, qty, typeOperation
+            return id, None, None, None, typeOperation
+        elif typeOperation == "limit" or typeOperation == "market":
+            side = list_order[1]
+            if typeOperation == "limit":
+                price, qty  = int(list_order[2]),  int(list_order[3])
+                return None, side, price, qty, typeOperation         
+            elif typeOperation == "market":
+                qty = int(list_order[2])
+                return None, side, None, qty, typeOperation  
